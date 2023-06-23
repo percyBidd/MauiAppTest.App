3c36fa7a791b4c4fce9ad577911a5495c4852c53
Pervical <percybidd@gmail.com>
2023-06-23 18:33:07 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace MauiAppTest;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+	private void OnResetClicked(object sender, EventArgs e)
+	{
+        var rand = new Random();
+        //Thread.Sleep(1000);
+		Int32 col1 = count * 3 - 100 / 3;
+		Int32 col2 = rand.Next(255);
+		Int32 col3 = rand.Next(255);
+		CounterBtnReset.BackgroundColor = Color.FromRgb(col1,col2,col3);
+		count =0;
+
+			CounterBtn.Text = $"Clicked {count} time";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
